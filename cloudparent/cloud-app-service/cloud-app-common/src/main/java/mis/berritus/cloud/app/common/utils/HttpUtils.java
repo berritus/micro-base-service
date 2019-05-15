fa9b529aebe93d8f47145b35c6392bd09f3fc2b5
berritus <95559215@qq.com>
2019-05-16 01:54:44 +0800

    add proj elasticsearch

--- cloudparent/cloud-app-service/cloud-app-common/src/main/java/mis/berritus/cloud/app/common/utils/HttpUtils.java
+++ cloudparent/cloud-app-service/cloud-app-common/src/main/java/mis/berritus/cloud/app/common/utils/HttpUtils.java
@@ -1,12 +1,12 @@
 package mis.berritus.cloud.app.common.utils;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.*;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -20,123 +20,170 @@ import java.util.Map;
 
 /**
  * HTTP工具类
- * 
+ *
  * @author jonathan 2018年4月12日 上午9:35:13
  * @since 1.0.0
  */
 public class HttpUtils {
 
-	public static final String DEFAULT_ENCODING = "UTF-8";
+    public static final String DEFAULT_ENCODING = "UTF-8";
 
-	public static String doGet(String url, Map<String, String> headers, Map<String, String> param) {
+    public static String doGet(String url, Map<String, String> headers, Map<String, String> param) {
 
-		// 创建Httpclient对象
-		CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 创建Httpclient对象
+        CloseableHttpClient httpclient = HttpClients.createDefault();
 
-		String resultString = "";
-		CloseableHttpResponse response = null;
-		try {
-			// 创建uri
-			URIBuilder builder = new URIBuilder(url);
-			if (param != null) {
-				for (String key : param.keySet()) {
-					builder.addParameter(key, param.get(key));
-				}
-			}
-			URI uri = builder.build();
+        String resultString = "";
+        CloseableHttpResponse response = null;
+        try {
+            // 创建uri
+            URIBuilder builder = new URIBuilder(url);
+            if (param != null) {
+                for (String key : param.keySet()) {
+                    builder.addParameter(key, param.get(key));
+                }
+            }
+            URI uri = builder.build();
 
-			// 创建http GET请求
-			HttpGet httpGet = new HttpGet(uri);
+            // 创建http GET请求
+            HttpGet httpGet = new HttpGet(uri);
 
-			// 设置header
-			setHeaders(httpGet, headers);
+            // 设置header
+            setHeaders(httpGet, headers);
 
-			// 执行请求
-			response = httpclient.execute(httpGet);
-			// 判断返回状态是否为200
-			if (response.getStatusLine().getStatusCode() == 200) {
-				resultString = EntityUtils.toString(response.getEntity(), DEFAULT_ENCODING);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (response != null) {
-					response.close();
-				}
-				httpclient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return resultString;
-	}
+            // 执行请求
+            response = httpclient.execute(httpGet);
+            // 判断返回状态是否为200
+            if (response.getStatusLine().getStatusCode() == 200) {
+                resultString = EntityUtils.toString(response.getEntity(), DEFAULT_ENCODING);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (response != null) {
+                    response.close();
+                }
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultString;
+    }
 
-	public static String doGet(String url, Map<String, String> param) {
-		return doGet(url, null, param);
-	}
+    public static String doGet(String url, Map<String, String> param) {
+        return doGet(url, null, param);
+    }
 
-	public static String doGet(String url) {
-		return doGet(url, null, null);
-	}
+    public static String doGet(String url) {
+        return doGet(url, null, null);
+    }
 
-	public static String doPost(String url, Map<String, String> headers, Map<String, String> param) {
-		// 创建Httpclient对象
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		CloseableHttpResponse response = null;
-		String resultString = "";
-		try {
-			// 创建Http Post请求
-			HttpPost httpPost = new HttpPost(url);
-			setHeaders(httpPost, headers);
-			// 创建参数列表
-			if (param != null) {
-				// 模拟表单
-				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(createNameValuePair(param));
-				httpPost.setEntity(entity);
-			}
-			// 执行http请求
-			response = httpClient.execute(httpPost);
-			resultString = EntityUtils.toString(response.getEntity(), DEFAULT_ENCODING);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				response.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+    public static String doPost(String url, Map<String, String> headers, Map<String, String> param) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
+        try {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+            setHeaders(httpPost, headers);
+            // 创建参数列表
+            if (param != null) {
+                // 模拟表单
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(createNameValuePair(param));
+                httpPost.setEntity(entity);
+            }
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), DEFAULT_ENCODING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-		return resultString;
-	}
+            if (httpClient != null) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-	public static String doPost(String url, Map<String, String> param) {
-		return doPost(url, null, param);
-	}
+        return resultString;
+    }
 
-	public static String doPost(String url) {
-		return doPost(url, null, null);
-	}
+    public static String doPost(String url, Map<String, String> param) {
+        return doPost(url, null, param);
+    }
 
-	public static void setHeaders(HttpRequestBase httpRequestBase, Map<String, String> headers) {
-		for (Map.Entry<String, String> entry : headers.entrySet()) {
-			httpRequestBase.addHeader(entry.getKey(), entry.getValue());
+    public static String doPost(String url) {
+        return doPost(url, null, null);
+    }
 
-		}
-	}
+    public static void setHeaders(HttpRequestBase httpRequestBase, Map<String, String> headers) {
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            httpRequestBase.addHeader(entry.getKey(), entry.getValue());
 
-	public static List<NameValuePair> createNameValuePair(Map<String, String> params) {
-		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-		for (Map.Entry<String, String> entry : params.entrySet()) {
-			String name = entry.getKey();
-			String value = entry.getValue();
+        }
+    }
 
-			nameValuePairs.add(new BasicNameValuePair(name, value));
+    public static List<NameValuePair> createNameValuePair(Map<String, String> params) {
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String name = entry.getKey();
+            String value = entry.getValue();
 
-		}
-		return nameValuePairs;
+            nameValuePairs.add(new BasicNameValuePair(name, value));
 
-	}
+        }
 
+        return nameValuePairs;
+    }
+
+    public static String doPut(String url, Map<String, String> headers, String jsonData) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
+        try {
+            // 创建Http Put请求
+            HttpPut httpPut = new HttpPut(url);
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectTimeout(35000).setConnectionRequestTimeout(35000)
+                    .setSocketTimeout(60000).build();
+            httpPut.setConfig(requestConfig);
+            httpPut.setHeader("Content-type", "application/json");
+            httpPut.setHeader("DataEncoding", "UTF-8");
+           //httpPut.setHeader("token", token);
+            httpPut.setEntity(new StringEntity(jsonData));
+            // 执行http请求
+            response = httpClient.execute(httpPut);
+            resultString = EntityUtils.toString(response.getEntity(), DEFAULT_ENCODING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (httpClient != null) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return resultString;
+    }
 }
---
+++ cloudparent/cloud-app-service/cloud-service-elasticsearch/src/main/java/mis/berritus/cloud/elasticsearch/conf/ElasticSearchApplication.java
@@ -0,0 +1,29 @@
+package mis.berritus.cloud.elasticsearch.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Description:
+ * @Copyright: mis520
+ * @Author: Qin Guihe
+ * @Date: Create in 2019/5/16
+ */
+@EnableEurekaClient
+@ComponentScan({"mis.berritus.cloud.elasticsearch"})
+@SpringBootApplication
+@EnableFeignClients(basePackages = {"mis.berritus.cloud.service.prod.feign.client"})
+@EnableHystrix//开启熔断器功能
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableHystrixDashboard//断路器仪表盘
+public class ElasticSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchApplication.class);
+    }
+}
